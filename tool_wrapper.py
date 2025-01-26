909fb45861e4efbfbe88bbf6c2e1ece8a79582b7
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-01-26 17:05:44 +0100

    refactor for tool_wrapper_file

--- tool_wrapper.py
+++ tool_wrapper.py
@@ -2,12 +2,15 @@ import os
 import subprocess
 from set_mode import is_manual_mode
 
-def wrapper(command, tool):
+def manual_mode_instructions(command, tool):
+    user_answer = input(f"Do you want to run {tool} with the following flags? : {command}\nor a customize it (y/else)\n")
+    if (user_answer.lower() == 'y'):
+        custom_flags = input()
+        return custom_flags
 
+def wrapper(command, tool):
     if (is_manual_mode):
-        user_answer = input(f"Do you want to run {tool} with the following flags? : {command}\nor a customize it (y/else)")
-        if (user_answer.lower() == 'y') :
-            custom_flags = input()
+        manual_mode_instructions(command, tool)
     
     try:
         result = subprocess.run(['which', tool], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
