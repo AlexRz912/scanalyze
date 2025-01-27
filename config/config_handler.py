bafe159c63d476e9716f0c1831cd0c7021fc0bc6
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-01-27 18:02:14 +0100

    implemented project_path setup from choosing a project feature

--- config/config_handler.py
+++ config/config_handler.py
@@ -1,9 +1,19 @@
 import os
 import json
 
-def set_working_path():
-    config_file = os.path.join("config", "config.json")
+def load_config_path():
+    return os.path.join("config", "config.json")
 
+def load_config_file(config_path):
+    """Charge le fichier JSON à partir du chemin donné."""
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Le fichier de configuration '{config_path}' est introuvable.")
+    
+    with open(config_path, 'r') as file:
+        return json.load(file)
+
+def set_working_path(config_file):
+    
     if not os.path.exists(config_file):
         raise FileNotFoundError(f"Config file '{config_path}' not found.")
 
@@ -17,3 +27,21 @@ def check_working_path_exists(path):
         print("No path was found, please provide a correct path value to working_path : config/config.json")
         return False
     return True
+
+def set_project_path(path, input):
+    return (f"{path}/{input}")
+
+def save_config(config_path, data):
+    """Écrit les modifications dans le fichier JSON."""
+    with open(config_path, 'w') as file:
+        json.dump(data, file, indent=4)  # `indent=4` pour une meilleure lisibilité
+
+# Modifier une clé dans la configuration
+def update_project_path(config_path, config_file, path):
+    
+    # Modifier la clé
+    config_file["project_path"] = path
+    
+    # Sauvegarder les changements
+    save_config(config_path, config_file)
+    print(f"Le chemin de travail a été mis à jour avec succès : {path}")
--- scanalyze.py
+++ scanalyze.py
@@ -6,7 +6,11 @@ from config import config_handler
 
 display_banner()
 
-working_path = config_handler.set_working_path()
+config_path = config_handler.load_config_path()
+config_file = config_handler.load_config_file(config_path)
+
+working_path = config_handler.set_working_path(config_path)
+project_path = ""
 # Code for loading working path
 
 
@@ -17,21 +21,17 @@ while (True):
     menu.display_start_menu()
     user_choice = input()
     
-    
-
     if (user_choice == '2'):
         print("\n")
 
         
         # code to load project
-
-
         # debug if no project exist
         projects_handler.list_projects(working_path)
         project = projects_handler.choose_projects("load")
         print(f"{project} selected, configuring environment...")
-
-
+        project_path = config_handler.set_project_path(working_path, project)
+        config_handler.update_project_path(config_path, config_file, project_path)
         # prepare environment
 
     elif (user_choice == '3'):
