ab784e987d0c1596995ebd109266b3d4d4b823ca
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-02-01 21:13:30 +0100

    implemented project config

--- config/config_loader.py
+++ config/config_loader.py
@@ -1,7 +1,6 @@
 from helpers import config_helper
 
 def load_config(config_type):
-    
     if config_type == "app":
         return config_helper.config_get(
             config_type,
@@ -14,6 +13,21 @@ def load_config(config_type):
             ]
         )
     elif config_type == "project":
-        return
+
+        config_path = config_helper.load_config_path("app")
+        config_file = config_helper.load_config_file(config_path)
+        project_path = config_helper.get_project_path(config_file)
+        print(project_path)
+        return config_helper.config_get(
+            config_type,
+            [
+                "",
+                "",
+                "",
+                "",
+                ""
+            ],
+            project_path
+        )
 
     
--- helpers/config_helper.py
+++ helpers/config_helper.py
@@ -1,26 +1,29 @@
 import os
 import json
 
-def load_config_path(config_type):
+def load_config_path(config_type, project_path=None):
     if (config_type == "app"):
         return os.path.join("config", "config.json")
     elif (config == "project"):
-        return 
+        return os.path.join(f"{project_path}/project_config/project_config.json")
 
-def load_config_file(config_path):
+def load_config_file(file_path):
     """Loads JSON from given file path"""
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Config file '{config_path}' not found")
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Config file '{file_path}' not found")
     
-    with open(config_path, 'r') as file:
+    with open(file_path, 'r') as file:
         return json.load(file)
 
-def config_get(config_type, keys=[]):
+def get_project_path(config):
+    return config.get("project_path")
+
+def config_get(config_type, keys=[], project_path=None):
     config = {}
-    config["config_path"] = load_config_path(config_type)
+    config["config_path"] = load_config_path(config_type, project_path)
     config["config_file"] = load_config_file(config["config_path"])
     for i in keys:
-        config[i] = config["config_file"].get(i)  # Ajoute la valeur de la clé i dans le dictionnaire config
+        config[i] = config["config_file"].get(i)  
     return config
 
 def set_project_path(path, input):
@@ -45,3 +48,27 @@ def update_project_path(config_path, config_file, working_path, project):
     # Save changes
     save_config(config_path, config_file)
     print(f"Project path successfully updated : {path}")
+
+
+# def init_project_config(data, project_path):
+    # os.system(f"mkdir {project_path}/project_config")
+    # os.system(f"touch {project_path}/project_config/project_config.json")
+# 
+    # config_path = "project_config/project_config.json"
+    # with open(config_path, 'w') as file:
+        # json.dump(data, file, indent=4)
+# 
+# data = {
+    # "project_name": "My Project",
+    # "settings": "{
+    # }
+    # "project_state": {
+        # "assets_found": 0,
+        # "live_domain_found": 0,
+        # "status_codes_received": 0,
+        # "technos_found": 0,
+        # "responses_received": 0,
+        # "header_overview": 0
+    # }",
+    # "domain_specific_folders": []
+# }
