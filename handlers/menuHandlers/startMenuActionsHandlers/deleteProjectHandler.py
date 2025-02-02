c1827bb35825ac61ef00a200631f53e785688b91
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-02-02 18:11:07 +0100

    reimplemented all of start menu actions after refactor

--- handlers/menuHandlers/startMenuActionsHandlers/deleteProjectHandler.py
+++ handlers/menuHandlers/startMenuActionsHandlers/deleteProjectHandler.py
@@ -1,3 +1,18 @@
+from helpers.configHelper import config_get
+from helpers.projectsHelper import delete_confirmation
+from helpers.projectsHelper import delete_project_on_confirmation
+from helpers.menuHelper import list_projects_into_action
+
 def delete_project_handler():
-    print("delete project")
-    return
+    config = config_get(
+            "app", 
+            [
+                "working_path",
+                "project_path"
+            ]
+        )
+    print("\n")
+    project = list_projects_into_action(config["working_path"], "delete")
+    delete_flag = delete_confirmation(project)
+    delete_project_on_confirmation(delete_flag, project)
+    
