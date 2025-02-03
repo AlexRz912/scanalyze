8f3c055403a4c84991e41661cfbce8a157baf485
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-02-03 23:07:19 +0100

    need to implement asset_recon and domain_recon modules

--- controllers/reconController.py
+++ controllers/reconController.py
@@ -5,8 +5,12 @@ from handlers.projectStateHandlers.setProjectStateHandler import *
 from handlers.toolsHandlers.runToolsHandler import run_tools_handler
 
 def recon_controller():
+
     config = load_config("app")
     project_config = load_config("project")
+
     set_project_state_handler(config["project_path"], project_config)
+
     state = get_project_state_handler(project_config["recon"])
+    
     run_tools_handler(state)
--- handlers/toolsHandlers/runToolsHandler.py
+++ handlers/toolsHandlers/runToolsHandler.py
@@ -6,8 +6,8 @@ def run_tools_handler(state):
     print(state)
     if not state["initial_asset_recon_completed"]: # supprimer la notion d'initial et indiquer que la recon est à refaire si il y a du pending recon
         print("starting_asset_recon ...")
-        # va devoir chercher le premier outil d'asset recon depuis la config
-        time.sleep(2)
+        asset_recon_module()
     if not state["initial_domain_recon_completed"]: # supprimer la notion d'initial et indiquer à refaire si pending_recon
         print("starting_domain_recon ... ")
+        domain_recon_module()
         # appelle domainReconModule en allant chercher le premier outil d'asset recon depuis
