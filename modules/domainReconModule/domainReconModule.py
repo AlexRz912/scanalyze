30b9cad5243095d2f79fb5889f6152a838d8aea0
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2025-02-04 01:52:00 +0100

    implemented domain recon module

--- modules/domainReconModule/domainReconModule.py
+++ modules/domainReconModule/domainReconModule.py
@@ -1,2 +1,17 @@
+import os
+import time
+from helpers.configHelper import config_get
+
+
 def start():
-    print("domain_recon_module")
+    tools = config_get("app", ["domain_recon_tools"])
+    path = config_get("app", ["project_path"])
+    project_path = path["project_path"]
+    for i in tools["domain_recon_tools"].keys():
+        os.system(f"mkdir {project_path}/{i}")
+        sh_exec(project_path, tools["domain_recon_tools"][i])
+
+def sh_exec(path, tool):
+    print(path)
+    tool = tool.replace("project_path", path)
+    os.system(tool)
